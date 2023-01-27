63b78bb27562997828365dbf6add3c049c94b544
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2023-01-27 06:58:03 -1200

    yes i write python too

--- Insertation Sort.py
+++ Insertation Sort.py
@@ -6,13 +6,7 @@ import random
 import re
 import sys
 
-#
-# Complete the 'insertionSort1' function below.
-#
-# The function accepts following parameters:
-#  1. INTEGER n
-#  2. INTEGER_ARRAY arr
-#
+
 
 def insertionSort1(n, arr):
     temp=arr[-1]
