dfb0256a2b25bf48445e840f811ee5d889a6ac94
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2023-01-28 00:42:24 -1200

    prefix sum using py

---
+++ subarraysum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        sums_so_far = defaultdict(int)
+        our_sum = 0
+        num_subarrays = 0
+        for v in nums:
+            our_sum += v
+            if our_sum == k:
+                num_subarrays += 1
+            if (our_sum - k) in sums_so_far:
+                num_subarrays += sums_so_far[our_sum - k]
+            sums_so_far[our_sum] += 1
+        return num_subarrays
